e4cb834150ecf4d8c9c57c89f0f056c40fd555fe
glodanm <glodan196915@gmail.com>
2023-10-21 13:14:20 +0300

    add tests

--- group/tests.py
+++ group/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import Group
 
-# Create your tests here.
+
+class GroupViewsTest(TestCase):
+    def setUp(self):
+        self.group = Group.objects.create(name="Test Group", description="Test Description")
+
+    def test_group_list_view(self):
+        response = self.client.get(reverse('groups-list'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'group-list.html')
+
+    def test_group_create_view(self):
+        response = self.client.get(reverse('group-add'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'group-form.html')
+
+    def test_group_update_view(self):
+        response = self.client.get(reverse('group-edit', kwargs={'pk': self.group.pk}))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'group-form.html')
+
+    def test_group_delete_view(self):
+        response = self.client.get(reverse('group-delete', kwargs={'pk': self.group.pk}))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'group-confirm-delete.html')
--- user/tests.py
+++ user/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import User
 
-# Create your tests here.
+
+class UserViewsTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(username="test_user")
+
+    def test_user_list_view(self):
+        response = self.client.get(reverse('users-list'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'user-list.html')
+
+    def test_user_create_view(self):
+        response = self.client.get(reverse('user-add'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'user-form.html')
+
+    def test_user_update_view(self):
+        response = self.client.get(reverse('user-edit', kwargs={'pk': self.user.pk}))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'user-form.html')
+
+    def test_user_delete_view(self):
+        response = self.client.get(reverse('user-delete', kwargs={'pk': self.user.pk}))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'user-confirm-delete.html')
